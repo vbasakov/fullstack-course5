1fc1abe09e14e41670b19b45f1b9e155fafd87b9
vbasakov <vbasakov@gmail.com>
2020-09-17 21:37:45 +0300

    web pages test

--- examples/Lecture19/prototype.js
+++ examples/Lecture19/prototype.js
@@ -1,32 +1,32 @@
 //**  Prototypal inheritance
-// var parent = {
-//   value: "parentValue",
-//   obj: {
-//     objValue: "parentObjValue"
-//   },
-//   walk: function () {
-//     console.log("walking!");
-//   }
-// };
-//
-// var child = Object.create(parent);
-// console.log("CHILD - child.value: ", child.value);
-// console.log("CHILD - child.obj.objValue: ", child.obj.objValue);
-// console.log("PARENT - parent.value: ", parent.value);
-// console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
-// console.log("parent: ", parent);
-// console.log("child: ", child);
-//
-// child.value = "childValue";
-// child.obj.objValue = "childObjValue";
-// console.log("*** CHANGED: child.value = 'childValue'");
-// console.log("*** CHANGED: child.obj.objValue = 'childObjValue'");
-// console.log("CHILD - child.value: ", child.value);
-// console.log("CHILD - child.obj.objValue: ", child.obj.objValue);
-// console.log("PARENT - parent.value: ", parent.value);
-// console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
-// console.log("parent: ", parent);
-// console.log("child: ", child);
+var parent = {
+  value: "parentValue",
+  obj: {
+    objValue: "parentObjValue"
+  },
+  walk: function () {
+    console.log("walking!");
+  }
+};
+
+var child = Object.create(parent);
+console.log("CHILD - child.value: ", child.value);
+console.log("CHILD - child.obj.objValue: ", child.obj.objValue);
+console.log("PARENT - parent.value: ", parent.value);
+console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
+console.log("parent: ", parent);
+console.log("child: ", child);
+
+child.value = "childValue";
+child.obj.objValue = "childObjValue";
+console.log("*** CHANGED: child.value = 'childValue'");
+console.log("*** CHANGED: child.obj.objValue = 'childObjValue'");
+console.log("CHILD - child.value: ", child.value);
+console.log("CHILD - child.obj.objValue: ", child.obj.objValue);
+console.log("PARENT - parent.value: ", parent.value);
+console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
+console.log("parent: ", parent);
+console.log("child: ", child);
 //
 // console.log("child.obj === parent.obj ? ", child.obj === parent.obj);
 //
